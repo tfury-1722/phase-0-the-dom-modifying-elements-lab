bd45d0356b64e6843638a2bf0d75de483971d5b5
christopher <cdnance1829@gmail.com>
2022-04-24 23:19:43 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'Chris Nance is the champion';
